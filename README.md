1e97d69d87622406d83b7d70c61f97ef4ecf0334
DreamingCodes <dev@dreaming.codes>
2022-05-05 13:27:12 +0200

    Fixed app.provide

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ app.use(VueEmitter2, {}//EventEmitter2 options)
 Then you can use EventEmitter2 like
 ```
 this.$eventemitter2.emit('event', data)
+//OR
+const eventemitter2 = inject('eventEmitter2');
+eventemitter2.emit('event', data)
 ```
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const VueEmitter2: Plugin = {
         const eventEmitter2 = new EventEmitter2(options);
         app.config.globalProperties.$eventEmitter2 = eventEmitter2;
 
-        app.provide('eventEmitter2', {eventEmitter2});
+        app.provide('eventEmitter2', eventEmitter2);
     }
 };
 export default VueEmitter2;

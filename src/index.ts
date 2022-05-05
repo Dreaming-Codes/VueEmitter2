cdc49d9b1be4b8ce0cb9b4977201401b3d9ef306
DreamingCodes <dev@dreaming.codes>
2022-05-05 13:13:14 +0200

    added app.provide

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import { App, Plugin } from 'vue';
 import type {ConstructorOptions} from "eventemitter2";
 import {EventEmitter2} from "eventemitter2";
 
-export const VueEmitter2: Plugin = {
+const VueEmitter2: Plugin = {
     install(app: App, options: ConstructorOptions) {
-        app.config.globalProperties.$eventemitter2 = new EventEmitter2(options);
+        const eventEmitter2 = new EventEmitter2(options);
+        app.config.globalProperties.$eventEmitter2 = eventEmitter2;
+
+        app.provide('eventEmitter2', {eventEmitter2});
     }
 };
+export default VueEmitter2;
